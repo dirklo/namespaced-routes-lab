3c1818154a5205470f84f67b829872c1d51efc06
Rick <dirklo@gmail.com>
2021-03-04 10:32:56 +0100

    commit tree 24b91ac46cab559541b09f4e13db601c617682de

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    @preference = Preference.first
+    if @preference.allow_create_songs == true
+      @song = Song.new
+      render new_song_url
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
